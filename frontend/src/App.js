edf87eb350882122b646bec6d43689be2c3f2563
eoinlynchcodes <eoin95lynch@gmail.com>
2020-10-16 19:49:40 +0100

    Go

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -65,11 +65,7 @@ function App() {
           </button>
           <ul className="categories">
             <li>
-              <Link to="/category/Pants">Pants</Link>
-            </li>
-
-            <li>
-              <Link to="/category/Shirts">Shirts</Link>
+              <Link to="">Nav Needed?</Link>
             </li>
           </ul>
         </aside>
