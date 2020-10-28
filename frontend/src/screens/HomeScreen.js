9a8bfdea6ad761a73522cb77c17b257b992d2123
eoinlynchcodes <eoin95lynch@gmail.com>
2020-10-28 15:56:18 +0000

    Go

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -189,7 +189,7 @@ function HomeScreen(props) {
                 <br />
 
                 <div className="formbox">
-                  <input type="Submit" />
+                  <input className="submitButton" type="Submit" />
                 </div>
               </form>
             </div>
@@ -362,37 +362,6 @@ function HomeScreen(props) {
                 Margaux call upon their experience daily to help them achieve
                 their goals as a leading sustainable food business.
               </p>
-            </div>
-            <hr className="bottomHR"/>
-          </section>
-
-          <section className="homepagebox">
-            <div className="coloredBox yellow">
-              <h2>Newsletter Signup</h2>
-            </div>
-            <div className="white">
-              <p>
-                ***Text about what our news letter contains and why we send it.
-              </p>
-              <form
-                action="https://gmail.us19.list-manage.com/subscribe/post?u=5f345ee5b52850316f8e104b4&amp;id=e9b9605d2b"
-                method="post"
-                id="mc-embedded-subscribe-form"
-                name="mc-embedded-subscribe-form"
-                class="validate"
-                target="_blank"
-                novalidate
-              >
-                <div>
-                  <label>Subscribe to our News Letter</label>
-                  <div>
-                    <input placeholder="your@email.here" />
-                    <div>
-                      <input type="submit" value="Subscribe" name="subscribe" />
-                    </div>
-                  </div>
-                </div>
-              </form>
             </div>
             <hr className="bottomHR"/>
           </section>
--- frontend/src/sectionsByEoin/Footer.js
+++ frontend/src/sectionsByEoin/Footer.js
@@ -1,65 +1,59 @@
 import React from "react";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 import "./Footer.css";
 
 function Footer() {
-  
   const history = useHistory();
 
   const gohome = (e) => {
-    history.push('/');
-  }
+    history.push("/");
+  };
 
   return (
     <footer className="footer">
-
       <div className="footerHeading changeMouse">
         <h1 onClick={gohome}>4 Hands Food Studio</h1>
       </div>
       <div className="links">
-
-      <form
-                action="https://gmail.us19.list-manage.com/subscribe/post?u=5f345ee5b52850316f8e104b4&amp;id=e9b9605d2b"
-                method="post"
-                id="mc-embedded-subscribe-form"
-                name="mc-embedded-subscribe-form"
-                class="validate"
-                target="_blank"
-                novalidate
-              >
-                <div>
-                  <label>Subscribe to our News Letter</label>
-                  <div>
-                    <input placeholder="your@email.here" />
-                    <div>
-                      <input type="submit" value="Subscribe" name="subscribe" />
-                    </div>
-                  </div>
-                </div>
-              </form>
+        <form
+          action="https://gmail.us19.list-manage.com/subscribe/post?u=5f345ee5b52850316f8e104b4&amp;id=e9b9605d2b"
+          method="post"
+          id="mc-embedded-subscribe-form"
+          name="mc-embedded-subscribe-form"
+          class="validate"
+          target="_blank"
+          novalidate
+        >
+          <div>
+            <label><u>Newsletter Signup</u></label>
+            <div><br/>
+              <input type="text" placeholder="your@email.here" />
+              <input type="submit" className="subscribeButton" value="Sign Up" name="subscribe" />
+            </div>
+          </div>
+        </form>
         <div className="">
-          <p><u>Follow us:</u></p>
+          <p>
+            <u>Follow us:</u>
+          </p>
           <a href="https://www.instagram.com/4hands_food_studio/?hl=en">
-            <p>
-              Instagram
-            </p>
+            <p>Instagram</p>
           </a>
           <a href="https://medium.com/@4handsf">
-            <p>
-              Medium
-            </p>
+            <p>Medium</p>
           </a>
         </div>
 
         <div className="">
-          <p><u>Read:</u></p>
+          <p>
+            <u>Read:</u>
+          </p>
           <a href="https://www.irishtimes.com/life-and-style/food-and-drink/restaurant-reviews/review-the-best-pop-up-restaurant-in-ireland-1.4194051">
             Our Irish Times article
           </a>
-          <br/><br/>
-          <a href="https://medium.com/@4handsf">
-            4 Hands Food Studio Blog
-          </a>
+          <br />
+          <br />
+          <a href="https://medium.com/@4handsf">4 Hands Food Studio Blog</a>
         </div>
 
         <nav>
@@ -67,13 +61,15 @@ function Footer() {
           <a href="#about">About</a>
           <a href="#contact">Contact</a>
           <a href="#retailers">Retailers</a>
-          <a href="#takeaways">Order Takeaways</a>
+          <a href="#takeaways">Takeaways</a>
         </nav>
       </div>
 
       <div className="bottom">
         <div className="">
-          <p><a>&copy; 4 Hands Food Studio 2020</a></p>
+          <p>
+            <a>&copy; 4 Hands Food Studio 2020</a>
+          </p>
         </div>
 
         <div className="">
