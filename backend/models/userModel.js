37a8c0a583694457d8792e835a9ac722e0e21432
eoinlynchcodes <eoin95lynch@gmail.com>
2020-11-25 17:46:30 +0000

    Go

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,9 +5,11 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String, required: true, unique: true, index: true, dropDups: true,
   },
-  password: { type: String, required: true },
+  password: { type: String, required: true, bcrypt: true },
   isAdmin: { type: Boolean, required: true, default: false },
 });
+
+userSchema.plugin(require('mongoose-bcrypt'));
 
 const userModel = mongoose.model('User', userSchema);
 
